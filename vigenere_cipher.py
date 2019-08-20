240e68ceec72342c34cae9814a492a704b6423ea
Shashwat Kadam <32579459+Shashwat4K@users.noreply.github.com>
2019-08-20 17:19:21 +0530

    Important Update made
    Added a function which calculates Vigenere cipher without using tableau.
    To do next: Add decryption function

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -2,12 +2,14 @@ from math import ceil
 import time
 
 universe = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-plain_dict = dict(zip(list(universe), range(26)))
-key_dict = dict(zip(list(universe.lower()), range(26)))
+character_dict = dict(zip(list(universe), range(26)))
+character_dict_inv = dict(enumerate(list(universe.lower())))
 tableau = list()
 
+# TODO: Encryption is working (len(key) <= len(plain_text)), function for decryption is to be added. 
+
 def get_coordinates(plain_character, key_character):
-    return (plain_dict[plain_character], key_dict[key_character])
+    return (character_dict[plain_character], character_dict[key_character.upper()])
 
 def create_vigenere_tableau():
     tableau.append(list(universe))
@@ -18,19 +20,34 @@ def get_vigenere_value(coordinates):
     p, k = coordinates
     return tableau[p][k]
 
-def vigenere_cipher(plain_text, key):
+def vigenere_cipher_using_tableau(plain_text, key):
     text_size = len(plain_text)
     block_size = len(key)
     result = ''
     for j in range(ceil(text_size/block_size)):
         offset = j * block_size
         for i in range(block_size):
-            if i + offset == text_size:
-                break    
-            coords = get_coordinates(plain_text[i+offset], key[i])
-            result += get_vigenere_value(coords)
-            
+            if i + offset < text_size:
+                coords = get_coordinates(plain_text[i+offset], key[i])
+                result += get_vigenere_value(coords)
+            else:
+                break
+
     return result
+
+def vigenere_cipher_without_tableau(plain_text, key):
+    text_size = len(plain_text)
+    block_size = len(key)
+    result = ''
+    for j in range(ceil(text_size / block_size)):
+        offset = j * block_size
+        for i in range(block_size):
+            if i+offset == text_size:
+                break
+            c = get_coordinates(plain_text[i+offset], key[i])
+            result = result + character_dict_inv[(c[0] + c[1]) % 26]
+    
+    return result.upper()       
 
 create_vigenere_tableau()
 
@@ -38,7 +55,13 @@ input_plain_text = input('Enter plain text: ')
 input_key = input('Enter key: ')
 
 start_time = time.time()
-print(vigenere_cipher(input_plain_text.upper(), input_key.lower()))
+print(vigenere_cipher_using_tableau(input_plain_text.upper(), input_key.lower()))
 end_time = time.time()
 
-print('Time taken = {} seconds'.format(end_time-start_time))
+print('Time taken (with tableu)= {} seconds'.format(end_time-start_time))
+
+start_time = time.time()
+print(vigenere_cipher_without_tableau(input_plain_text.upper(), input_key.lower()))
+end_time = time.time()
+
+print('Time taken (without tableu)= {} seconds'.format(end_time-start_time))
